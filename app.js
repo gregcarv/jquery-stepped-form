a5e4767fff61fb669c8a9cb69e936c214632c9d3
Gregory Carvalhal <gregory.carvalhal@gmail.com>
2019-06-19 15:42:02 -0400

    Update app.js
    Added comments

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
-jq$ = jQuery.noConflict();
+$('.form .section:first-child').show().siblings().hide(); // hide all sections except first section
 
-jq$('.form .section:first-child').show().siblings().hide();
-
-
-jq$('.form .section button').click(function(e){
-  const hasNext = jq$(this).parents('.section').next('.section').length;
+$('.form .section button').click(function(e){ // add click event to 'Next' button
+  const hasNext = $(this).parents('.section').next('.section').length; // check if the active element has a next sibling
+  
   if (hasNext > 0) {
-    jq$(this).parents('.section').hide().next('.section').show();
+    $(this).parents('.section').hide().next('.section').show(); // hide this element and show the next element 
   }
-})
+});
 
-
-jq$('.form .section .back').click(function (e) {
-  const hasPrevious = jq$(this).parents('.section').prev('.section').length;
+$('.form .section .back').click(function (e) { // add click event to 'Back' link
+  const hasPrevious = $(this).parents('.section').prev('.section').length; // check if the active element has a previous sibling
+  
   if (hasPrevious > 0){
-    jq$(this).parents('.section').hide().prev('.section').show();
+    jq$(this).parents('.section').hide().prev('.section').show(); // hide this element and show the previous slement
   }
-})
+});
